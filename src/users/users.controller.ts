fd8da5197e4b65f0eb8c937aef78c709cdbeee87
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-23 16:18:58 +0200

    First method using the provider

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -30,8 +30,7 @@ export class UsersController {
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(getUserParamDto);
-    return 'You sent a get req to users endpoint';
+    return this.usersService.findAll(getUserParamDto, limit, page);
   }
 
   @Post()
