e9e230c5ccf7df0ecd0c611f6385ef8b92724d63
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-23 10:39:55 +0200

    Add a global validation pipe + fix get and post accordingly

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,28 +9,36 @@ import {
   Query,
   ParseIntPipe,
   DefaultValuePipe,
+  ValidationPipe,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 // To make parameters optional, use the ? operator
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
-
-    @Query('limit', new DefaultValuePipe(1), ParseIntPipe) limit: number,
+    @Param() getUserParamDto: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(id);
-    console.log(limit);
-
+    console.log(getUserParamDto);
     return 'You sent a get req to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() request: any, @Headers() headers: any) {
-    console.log(request);
+  public createUsers(
+    // Without the global validation
+    // @Body(new ValidationPipe()) createUserDto: CreateUserDto,
+
+    // With
+    @Body() createUserDto: CreateUserDto,
+    @Headers() headers: any,
+  ) {
+    console.log(createUserDto);
     console.log(headers);
-    const { firstName } = request;
+    const { firstName } = createUserDto;
     console.log(firstName);
     return 'You sent a post req to users endpoint';
   }
