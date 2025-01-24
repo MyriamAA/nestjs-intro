f22f00a26a21abf9d01a814de0ab02c73bc1fd7e
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-24 13:21:40 +0200

    Basic swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Add the validations everywhere instead of doing it per controller
@@ -12,6 +12,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger config
+
+  const config = new DocumentBuilder().setVersion('1.0').build();
+
+  // Instantiate a document object
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // First argument is the path
+  // Second argument is the app
+  // Third is the document
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
