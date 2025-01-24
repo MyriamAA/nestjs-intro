7e5bbe62cde9810cd6961ae200bf54157a7f23f5
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-24 11:01:25 +0200

    Add commands used in the creation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
 import { Controller } from '@nestjs/common';
-
+// nest generate controller auth --no-spec
 @Controller('auth')
 export class AuthController {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
-
+// nest generate module auth
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
