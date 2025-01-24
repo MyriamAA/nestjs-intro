7af7f66492ddf2014ed02d647e624c930f23d97f
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-24 13:53:28 +0200

    Add tags to the get users endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,7 +16,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { UsersService } from './providers/users.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 // To make parameters optional, use the ? operator
 @Controller('users')
@@ -27,6 +27,29 @@ export class UsersController {
     private readonly usersService: UsersService,
   ) {}
   @Get('/:id?')
+  @ApiOperation({
+    summary: 'Fetches a list of registered users on the application',
+  })
+  // We can add multiple api response
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched successfully based on the query',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'number',
+    required: false,
+    description: 'The number of entries returned per query',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+    description:
+      'The postition of the page number that you want the API to return',
+    example: 1,
+  })
   public getUsers(
     @Param() getUserParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
