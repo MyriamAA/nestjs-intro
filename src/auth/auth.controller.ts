0165606586ed0399a5f6f41e14b13748d9308142
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-24 11:33:48 +0200

    Inject the auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './providers/auth.service';
 // nest generate controller auth --no-spec
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    // Injecting the auth service
+    private readonly authService: AuthService,
+  ) {}
+}
