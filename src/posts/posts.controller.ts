9fa132dffe305e9923a464f56a8116740a300c2a
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-27 12:54:26 +0200

    Added decorators for APIs and patch request

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dto/create-post.dto';
+import { PatchPostDto } from './dto/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -15,10 +16,27 @@ export class PostsController {
   public getPosts(@Param('userId') userId: string) {
     return this.postsService.findAll(userId);
   }
-
+  @ApiOperation({
+    summary: 'Creates a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your post is created sucessfully',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
-    return 'You sent a post req to the post endpoint';
+  }
+
+  @ApiOperation({
+    summary: 'Updates an existing blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your post is updated sucessfully',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostsDto: PatchPostDto) {
+    console.log(patchPostsDto);
   }
 }
