eb04679d53d2f0b30d5a51c4788744926de82e7b
MyriamAA <myriamabouatmeh@gmail.com>
2025-01-27 11:01:29 +0200

    Add documentation for the DTO

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -14,18 +14,27 @@ import {
 import { PostType } from '../enums/post-type.enum';
 import { PostStatus } from '../enums/post-status.enum';
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 
 export class CreatePostDto {
+  @ApiProperty()
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    enum: PostType,
+    description: "Possible values  'post', 'page', 'story', 'series'",
+  })
   @IsEnum(PostType)
   @IsNotEmpty()
-  postType: string;
+  postType: PostType;
 
+  @ApiProperty({
+    description: "For example 'my-url'",
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -34,34 +43,62 @@ export class CreatePostDto {
   })
   slug: string;
 
+  @ApiProperty({
+    enum: PostStatus,
+    description: "Possible values 'draft', 'scheduled', 'review', 'published'",
+  })
   @IsEnum(PostStatus)
   @IsNotEmpty()
   status: PostStatus;
 
   @IsString()
+  @ApiPropertyOptional()
   @IsOptional()
-  content: string;
+  content?: string;
 
+  @ApiPropertyOptional({
+    description:
+      'Serialize your JSON object else a validation error will be thrown',
+  })
   @IsOptional()
   @IsJSON()
-  schema: string;
+  schema?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsUrl()
-  featuredImageUrl: string;
+  featuredImageUrl?: string;
 
-  @IsISO8601() /* Date format */
+  @ApiProperty({
+    description: 'Must be a valid timestamp in ISO8601',
+    example: '2024-03-16T07:46:32+0000',
+  })
+  @IsISO8601()
   @IsOptional()
-  publishOn: Date;
+  publishOn?: Date;
 
+  @ApiPropertyOptional()
   @IsArray()
   @IsOptional()
-  @IsString({
-    each: true,
-  }) /* To signify that each value in the array is a string */
-  @MinLength(3, { each: true }) // Each value should have a length of 3
-  tags: string[];
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
+  tags?: string[];
 
+  @ApiPropertyOptional({
+    type: 'array',
+    required: false,
+    items: {
+      type: 'object',
+      properties: {
+        key: {
+          type: 'string',
+        },
+        value: {
+          type: 'string',
+        },
+      },
+    },
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
@@ -70,5 +107,5 @@ export class CreatePostDto {
   // Matches the incoming req to the dto
   // Creates an instance of the dto
   // All the properties are validated against the dto
-  metaOptions: CreatePostMetaOptionsDto[];
+  metaOptions?: CreatePostMetaOptionsDto[];
 }
