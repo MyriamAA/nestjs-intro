ddd0620f0c4c4cb2975320aba591770e5a626e0d
MyriamAA <myriamabouatmeh@gmail.com>
2025-02-18 17:40:01 +0200

    Installing config module for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
+import { ConfigModule } from '@nestjs/config';
 
 // Run this in cmd pg_ctl start -D "C:\users\mfabouatmeh\Desktop\CodesDar\more\Postgre\data"
 @Module({
@@ -15,6 +16,9 @@ import { MetaOptionsModule } from './meta-options/meta-options.module';
     UsersModule,
     PostsModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       imports: [],
       inject: [],
