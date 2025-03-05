d830014f646ef9d1c8748a6c51b39e9dd05bee7b
MyriamAA <myriamabouatmeh@gmail.com>
2025-03-05 11:29:55 +0200

    Start of the paginated response

--- src/common/pagination/providers/pagination.provider.ts
+++ src/common/pagination/providers/pagination.provider.ts
@@ -1,16 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 import { ObjectLiteral, Repository } from 'typeorm';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(
+    /**
+     * Injecting request
+     */
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
   public async paginateQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
   ) {
-    return await repository.find({
+    const results = await repository.find({
       skip: (paginationQuery.page - 1) * paginationQuery.limit,
       take: paginationQuery.limit,
     });
+    /**
+     * Create the request URL
+     */
+
+    const baseUrl =
+      this.request.protocol + '://' + this.request.headers.host + '/';
+    const newUrl = new URL(this.request.url, baseUrl);
+
+    console.log(newUrl);
+
+    /**
+     * Calculalting page number
+     */
+    const totalItems = await repository.count();
+    const totalPages = Math.ceil(totalItems / paginationQuery.limit);
+    const nextPage =
+      paginationQuery.page == totalPages
+        ? paginationQuery.page
+        : paginationQuery.page + 1;
+
+    const previousPage =
+      paginationQuery.page == 1
+        ? paginationQuery.page
+        : paginationQuery.page - 1;
+    return results;
   }
 }
