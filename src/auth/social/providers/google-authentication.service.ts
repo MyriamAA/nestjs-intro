9ec31d93a5052e0dfd3121092cccd68e05fb35d3
MyriamAA <myriamabouatmeh@gmail.com>
2025-03-09 23:41:30 +0200

    Make the google auth provider start directly

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  constructor(
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+  private oauthClient: OAuth2Client;
+
+  onModuleInit() {
+    const clientId = this.jwtConfiguration.googleClientId;
+    const clientSecret = this.jwtConfiguration.googleClientSecret;
+
+    this.oauthClient = new OAuth2Client(clientId, clientSecret);
+  }
+}
