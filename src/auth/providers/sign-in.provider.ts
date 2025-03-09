3e02f8ebc9ed29d5b35df66ea92edcd3fdb300ce
MyriamAA <myriamabouatmeh@gmail.com>
2025-03-09 12:36:56 +0200

    Incorporate the generateTokensProvider into the signInProvider

--- src/auth/providers/sign-in.provider.ts
+++ src/auth/providers/sign-in.provider.ts
@@ -12,6 +12,7 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
 import { ActiveUserData } from '../interfaces/active-user-data.interface';
+import { GenerateTokensProvider } from '../providedrs/generate-tokens.provider';
 
 @Injectable()
 export class SignInProvider {
@@ -28,15 +29,9 @@ export class SignInProvider {
     private readonly hashingProvider: HashingProvider,
 
     /**
-     * Inject jwtService
+     * Inject generateTokensProvider
      */
-    private readonly jwtService: JwtService,
-
-    /**
-     * Inject jwtConfiguration
-     */
-    @Inject(jwtConfig.KEY)
-    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private readonly generateTokensProvider: GenerateTokensProvider,
   ) {}
   public async signIn(signInDto: SignInDto) {
     // Find the user using email ID
@@ -62,15 +57,6 @@ export class SignInProvider {
       throw new UnauthorizedException('Incorrect password');
     }
 
-    const accessToken = await this.jwtService.signAsync(
-      { sub: user.id, email: user.email } as ActiveUserData,
-      {
-        audience: this.jwtConfiguration.audience,
-        issuer: this.jwtConfiguration.issuer,
-        secret: this.jwtConfiguration.secret,
-        expiresIn: this.jwtConfiguration.accessTokenTtl,
-      },
-    );
-    return { accessToken };
+    return await this.generateTokensProvider.generateTokens(user);
   }
 }
