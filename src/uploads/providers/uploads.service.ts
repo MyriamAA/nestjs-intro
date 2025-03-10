6ce8201fdc67618e2e75566375c26db780b64126
MyriamAA <myriamabouatmeh@gmail.com>
2025-03-10 21:31:34 +0200

    Complete fileUpload method

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,8 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Upload } from '../upload.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UploadToAwsProvider } from './upload-to-aws.provider';
+import { ConfigService } from '@nestjs/config';
+import { UploadFileInterface } from '../interfaces/upload-file.interface';
+import { fileTypes } from '../enums/file-types.enum';
 
 @Injectable()
 export class UploadsService {
+  constructor(
+    /**
+     * Inject uploadsRepo
+     *  */
+    @InjectRepository(Upload)
+    private readonly uploadsRepository: Repository<Upload>,
+
+    /**
+     * Inject uploadToAwsProvider
+     */
+
+    private readonly uploadToAwsProvider: UploadToAwsProvider,
+
+    private readonly configService: ConfigService,
+  ) {}
+
   public async uploadFile(file: Express.Multer.File) {
-    // Upload to the file to AWS S3 bucket
+    try {
+      // Throw an error if unsupported type
+      if (
+        !['image/gif', 'image/jpeg', 'image/jpg', 'image/png'].includes(
+          file.mimetype,
+        )
+      ) {
+        throw new BadRequestException('Mime Type not supported');
+      }
+      // Upload to the file to AWS S3 bucket
+      const name = await this.uploadToAwsProvider.fileUpload(file);
+
+      // Generate to a new entry in db
+      const uploadFile: UploadFileInterface = {
+        name: name,
+        path: `https://${this.configService.get('appConfig.awsCloudFrontUrl')}/${name}}`,
+        type: fileTypes.IMAGE,
+        mime: file.mimetype,
+        size: file.size,
+      };
+
+      const upload = this.uploadsRepository.create(uploadFile);
+
+      return await this.uploadsRepository.save(upload);
+    } catch (error) {
+      throw new ConflictException(error);
+    }
   }
 }
--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './providers/uploads.service';
 import { UploadToAwsProvider } from './providers/upload-to-aws.provider';
+import { Upload } from './upload.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [UploadsController],
   providers: [UploadsService, UploadToAwsProvider],
+  imports: [TypeOrmModule.forFeature([Upload])],
 })
 export class UploadsModule {}
