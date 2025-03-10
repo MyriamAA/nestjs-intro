95ecf8fda17b6d7b85b84ab2f916bf6221f900cb
MyriamAA <myriamabouatmeh@gmail.com>
2025-03-10 20:56:51 +0200

    Setup basics of controller and service

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadsService {}
+export class UploadsService {
+  public async uploadFile(file: Express.Multer.File) {
+    // Upload to the file to AWS S3 bucket
+  }
+}
