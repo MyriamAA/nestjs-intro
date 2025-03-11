71b4aec39d316a9b91391db02b9048b8ee8cf91b
MyriamAA <myriamabouatmeh@gmail.com>
2025-03-11 15:37:31 +0200

    Initial template and senUserWelcome method

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,4 +1,29 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(
+    /**
+  Inject mailService */
+
+    private readonly mailerService: MailerService,
+  ) {}
+
+  public async sendUserWelcome(user: User): Promise<void> {
+    // Mail module default "from" is the no reply email
+    await this.mailerService.sendMail({
+      to: user.email,
+      from: `Onboarding Team <support@nestjs-blog.com>`,
+      subject: 'Welcome to nestjs blog',
+      template: './welcome',
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: 'http://localhost:3000',
+      },
+      // bcc: 'myriam.abouatmeh@dar.com',
+    });
+  }
+}
